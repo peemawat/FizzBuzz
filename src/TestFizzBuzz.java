dc04143b59dfec97e9d756a7dea6816e9e9017d6
peemawat <56067861+peemawat@users.noreply.github.com>
2019-10-05 19:37:57 +0700

    Test3
    Edit dicision of class and add Test3

--- src/TestFizzBuzz.java
+++ src/TestFizzBuzz.java
@@ -4,17 +4,23 @@ import org.junit.jupiter.api.Test;
 
 class TestFizzBuzz {
 	FizzBuzz fizzBuzz = new FizzBuzz();
-	private int integer = 0;
-	public void setInput() {
-		fizzBuzz.setInt(1);
-		integer = fizzBuzz.getInt();
-	}
 	
 	@Test
-	void test() {
-		setInput();
-		assertEquals(1, integer);
+	void test1() {
+		fizzBuzz.setInput();
+		assertEquals("1", fizzBuzz.getInput());
 		
+	}
+	@Test
+	void test2() {
+		fizzBuzz.setInput();
+		assertEquals("2", fizzBuzz.getInput());
+	}
+
+	@Test
+	void test3() {
+		fizzBuzz.setInput();
+		assertEquals("Fizz", fizzBuzz.getInput());
 	}
 
 }
