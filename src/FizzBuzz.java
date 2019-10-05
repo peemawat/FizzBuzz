3321bbf899a9331ce63fa4d90945bd64f3c11e8d
peemawat <56067861+peemawat@users.noreply.github.com>
2019-10-05 19:40:05 +0700

    add "Test3"
    This reverts commit add Test3
    add Test3 and edit class

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,29 +1,12 @@
 import java.util.Scanner;
 
 public class FizzBuzz {
-	private static int index = 0,input = 0;
-	public String getInput() {
-		if((input%3 == 0 )&(input%5 == 0) ) {
-			return "FizzBuzz";
-		}else if(input%5 == 0) {
-			return "Buzz";
-		}else if(input%3 == 0) {
-			return "Fizz";
-		}else {
-		return input+"";
-		}
+	int integer;
+	public void setInt(int i) {
+		integer = i;
 	}
-		public static int setInput() {
-			while(index < 15) {
-				index++;
-				input++;
-				if(input == index) {
-					break;
-				}
-				
-				
-			}
-			return input;
-		}
+	public int getInt() {
+		return integer;
+	}
 
 }
